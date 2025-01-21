1b32c48aef8e1436006969ce2cf8e51c22195943
TwoEightNine <twoeightnine@list.ru>
2025-01-21 08:06:54 +0400

    update version to 0.1.2 (3)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "global.msnthrp.learnmokshan"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
         resourceConfigurations.addAll(listOf("en", "ru"))
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
